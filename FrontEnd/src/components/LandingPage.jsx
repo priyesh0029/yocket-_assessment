b4df692ab6477e51488407d10b68f7ccc6b1aed0
Priyesh ML <priyeshvayala@gmail.com>
2024-06-25 07:57:50 +0530

    added some more description at landing page and cityDetails and vehicle details in the constants file

--- FrontEnd/src/components/LandingPage.jsx
+++ FrontEnd/src/components/LandingPage.jsx
@@ -1,7 +1,8 @@
 import { useDispatch } from "react-redux";
 import { setStage } from "../store/slices/gameSlice";
 import { LANDING_PAGE_IMAGE } from "../constants/imageUrl";
-import { useState } from "react"; 
+import { useState } from "react";
+import { landingPageDetails } from "../constants/landingPageDetails";
 
 const LandingPage = () => {
   const dispatch = useDispatch();
@@ -17,13 +18,18 @@ const LandingPage = () => {
       style={{ backgroundImage: `url(${LANDING_PAGE_IMAGE})` }}
     >
       <div className="text-center">
-        <h1 className="text-4xl font-bold text-white mt-36 animate-pulse">
+        <h1 className="md:text-4xl text-2xl font-bold text-white md:mt-36 mt-28 animate-pulse">
           WELCOME TO MANHUND:{" "}
           <span className="text-yellow-900">THE SHADOWED PURSUIT</span>
         </h1>
+        <h1 className="text-xs bg-black bg-opacity-50 p-2 font-semibold md:mx-16 mt-6 mx-4 rounded-xl text-white md:mt-12">
+          {landingPageDetails.desc}
+        </h1>
         <button
           className={`bg-yellow-900 md:text-2xl text-md  font-bold md:py-3 md:px-10 py-1 px-8 rounded-2xl bottom-0 md:right-4 right-0 absolute m-8  transition duration-300 ease-in-out ${
-            hovered ? "hover:bg-yellow-700 scale-110 text-black" : "hover:bg-yellow-800 text-white"
+            hovered
+              ? "hover:bg-yellow-700 scale-110 text-black"
+              : "hover:bg-yellow-800 text-white"
           }`}
           onClick={startGame}
           onMouseEnter={() => setHovered(true)}
--- FrontEnd/src/constants/vehicleDetails.js
+++ FrontEnd/src/constants/vehicleDetails.js
@@ -1,5 +1,4 @@
 export const vehicleDescription = {
-    title: "Select a Vehicle",
-    desc: "Select a vehicle for the cop to travel.The vehicle must have the range for a round to the city selected by the cop.",
-  };
-  
+  title: "Select a Vehicle",
+  desc: "Select a vehicle for the cop to travel.The vehicle must have the range for a round trip to the city which is selected by the cop.",
+};
